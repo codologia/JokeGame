7b953192c8ef9810b5e3450ba972b3a23a383742
codologia <77449618+codologia@users.noreply.github.com>
2021-01-21 18:12:14 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -18,20 +18,23 @@ class Point:
 class Button:
     UP = 0 # Кнопка не нажата
     DOWN = 1 # Кнопка нажата
-    def __init__(self, color, pos, width = 80, height = 30):
+    def __init__(self, color, position, width = 80, height = 30):
         self.color = color
-        self.pos = pos
+        self.position = position
         self.width = width
         self.height = height
         state = Button.UP # Состояние кнопки
     
     def draw(self):
-        pg.draw.rect(screen, self.color, (self.pos.x, self.pos.y,
+        pg.draw.rect(screen, self.color, (self.position.x, self.position.y,
                                           self.width, self.height))
+        
+    def jumpto(self, position):
+        self.position = position
 
     def is_in(self, point):
-        A = self.pos
-        C = Point(self.pos.x + self.width, self.pos.y + self.height)
+        A = self.position
+        C = Point(self.position.x + self.width, self.position.y + self.height)
         return A.x <= point.x <= C.x and A.y <= point.y <= C.y
 
 btn_yes = Button(color.RED, Point(50, 100)) # Создание кнопки YES
@@ -50,7 +53,7 @@ while running:
             mouse_pos = Point(event.pos[0], event.pos[1]) # Позиция мышки
             # Если мышка попала на кнопку
             if btn_no.is_in(mouse_pos):
-                pass
+                btn_no.jumpto(Point(200, 250))
             
     btn_yes.draw() # Рисование кнопки YES
     btn_no.draw()  # Рисование кнопки NO
